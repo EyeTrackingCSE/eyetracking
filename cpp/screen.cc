51adf1a10aae71118de04bb9d95a0c0ba43c1fd0
vardaro <anthonyavardaro67@gmail.com>
2020-12-20 16:15:14 -0600

    Wrapping C++ object in gyp

--- cpp/screen.cc
+++ cpp/screen.cc
@@ -1,6 +1,6 @@
 #include "screen.h"
 
-Screen::Screen(float h, float w)
+Screen::Screen(float w, float h)
 {
     Screen::height = h;
     Screen::width = w;
@@ -13,34 +13,104 @@ Screen::Screen(float h, float w)
     Screen::tobii->CoordinateTransformSetOriginOffset(Screen::offset, Screen::offset);
 }
 
-float Screen::GetHeight()
+/**
+ * Binds the Screen object to v8, so it can be created
+ * from a node process.
+ * */
+void Screen::Init(v8::Local<v8::Object> exports)
 {
-    return Screen::height;
+    v8::Isolate *isolate = exports->GetIsolate();
+    v8::Local<v8::Context> context = isolate->GetCurrentContext();
+
+    // Enable Screen object to be created at runtime by v8.
+    v8::Local<v8::ObjectTemplate> addon_data_tpl = v8::ObjectTemplate::New(isolate);
+    addon_data_tpl->SetInternalFieldCount(5);
+    v8::Local<v8::Object> addon_data = addon_data_tpl->NewInstance(context).ToLocalChecked();
+
+    // Tie the Screen constructor to Screen::New
+    v8::Local<v8::FunctionTemplate> tpl = v8::FunctionTemplate::New(isolate, Screen::New, addon_data);
+    tpl->SetClassName(v8::String::NewFromUtf8(isolate, "Screen").ToLocalChecked());
+
+    // Set Screen prototype functions
+    NODE_SET_PROTOTYPE_METHOD(tpl, "GetHeight()", Screen::GetHeight);
 }
 
-void Screen::SetHeight(float h)
+/**
+ * JS constructor for creating a new Screen object as
+ * let screen = new Screen(1920.0, 1080.0);
+ * */
+void Screen::New(const v8::FunctionCallbackInfo<v8::Value> &args)
 {
-    Screen::height = h;
+    v8::Isolate *isolate = args.GetIsolate();
+    v8::Local<v8::Context> context = isolate->GetCurrentContext();
+
+    // Screen must be called with the new keyboard
+    // i.e. let myScreen = new Screen(1920, 1080)
+    if (!args.IsConstructCall())
+    {
+        std::cout << "Please init Screen with the 'new' keyword" << std::endl;
+        return;
+    }
+
+    // Get the width
+    float w = 0.0f;
+    if (!args[0]->IsUndefined())
+        w = args[0]->NumberValue(context).FromMaybe(0.0f);
+
+    // Get the height;
+    float h = 0.0f;
+    if (!args[1]->IsUndefined())
+        h = args[1]->NumberValue(context).FromMaybe(0.0f);
+
+    Screen * s = new Screen(w, h);
+    s->Wrap(args.This());
+    args.GetReturnValue().Set(args.This());
 }
 
-float Screen::GetWidth()
+void Screen::GetHeight(const v8::FunctionCallbackInfo<v8::Value> &args)
 {
-    return Screen::width;
+    v8::Isolate *isolate = args.GetIsolate();
+
+    Screen *s = ObjectWrap::Unwrap<Screen>(args.Holder());
+
+    v8::Local<v8::Number> height = v8::Number::New(isolate, s->height);
+
+    args.GetReturnValue().Set(height);
 }
 
-void Screen::SetWidth(float w)
+void Screen::Listen(const v8::FunctionCallbackInfo<v8::Value> &args)
 {
-    Screen::width = w;
 }
 
-void Screen::AddRectangle(float x, float y, float w, float h, int id) {
-    IL::InteractorId rect_id = id;
+// float Screen::GetHeight()
+// {
+//     return Screen::height;
+// }
 
-    IL::Rectangle rect = {x, y, w, h};
+// void Screen::SetHeight(float h)
+// {
+//     Screen::height = h;
+// }
 
-    Screen::tobii->BeginInteractorUpdates();
+// float Screen::GetWidth()
+// {
+//     return Screen::width;
+// }
 
-    Screen::tobii->AddOrUpdateInteractor(rect_id, rect, 0.0f);
+// void Screen::SetWidth(float w)
+// {
+//     Screen::width = w;
+// }
 
-    Screen::tobii->CommitInteractorUpdates();
-}
+// void Screen::AddRectangle(float x, float y, float w, float h, int id)
+// {
+//     IL::InteractorId rect_id = id;
+
+//     IL::Rectangle rect = {x, y, w, h};
+
+//     Screen::tobii->BeginInteractorUpdates();
+
+//     Screen::tobii->AddOrUpdateInteractor(rect_id, rect, 0.0f);
+
+//     Screen::tobii->CommitInteractorUpdates();
+// }
--- cpp/screen.h
+++ cpp/screen.h
@@ -6,10 +6,13 @@
 #define SCREEN_H
 
 #include <vector>
+#include <iostream>
+#include <node.h>
+#include <node_object_wrap.h>
 #include <interaction_lib/InteractionLib.h>
 #include <interaction_lib/misc/InteractionLibPtr.h>
 
-class Screen
+class Screen : public node::ObjectWrap
 {
 private:
     float height;
@@ -18,19 +21,34 @@ private:
     std::vector<IL::Rectangle> rectangles;
     IL::UniqueInteractionLibPtr tobii;
 
+    Screen(float h, float w);
+    ~Screen();
+
+    static void New(const v8::FunctionCallbackInfo<v8::Value>& args);
+
+    static void GetHeight(const v8::FunctionCallbackInfo<v8::Value>& args);
+    static void SetHeight(const v8::FunctionCallbackInfo<v8::Value>& args);
+
+    static void SetWidth(const v8::FunctionCallbackInfo<v8::Value>& args);
+    static void GetWidth(const v8::FunctionCallbackInfo<v8::Value>& args);
+
+    static void AddRectangle(const v8::FunctionCallbackInfo<v8::Value>& args);
+
+    static void Listen(const v8::FunctionCallbackInfo<v8::Value>& args);
 
 public:
-    Screen(float h, float w);
-    
-    float GetHeight();
-    void SetHeight(float h);
 
-    float GetWidth();
-    void SetWidth(float w);
+    static void Init(v8::Local<v8::Object> exports);
 
-    void AddRectangle(float x, float y, float w, float h, int id);
+    // float GetHeight();
+    // void SetHeight(float h);
 
-    void Listen();
+    // float GetWidth();
+    // void SetWidth(float w);
+
+    // void AddRectangle(float x, float y, float w, float h, int id);
+
+    // void Listen(const v8::FunctionCallbackInfo<v8::Value> &args);
 };
 
 #endif // SCREEN_H
